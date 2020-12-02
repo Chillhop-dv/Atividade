3f24d2a2c8ca790ba5a1c60e2b75eaa15aca5989
Chillhop-dv <69175114+Chillhop-dv@users.noreply.github.com>
2020-12-01 23:06:43 -0300

    Update CRUD.php

--- Atividade/CRUD.php
+++ Atividade/CRUD.php
@@ -1,6 +1,6 @@
 <?php
 	
-$server = "de1tmi3t63foh7fa.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	";
+$server = "de1tmi3t63foh7fa.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
 $user = "gy3exai7wk7e9sg0";
 $password = "llo3oub1rpdp2rqe";
 $banco = "prvf4e1byhpdus1l";
